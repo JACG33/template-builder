a1b19fec8a32d36593ebcd0d86292319a3b6e526
JACG33 <casanasjoe@gmail.com>
2024-04-04 13:11:20 -0400

    Agregando lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
+import { Suspense, lazy } from "react"
 import { BuilderArea } from "./components/BuilderArea"
-import EditorTools from "./tools/EditorTools"
+const EditorTools = lazy(() => import("./tools/EditorTools"))
 import { useEditorProvider } from "./hoks/useEditorProvider"
-import SideBarElementsItems from "./components/SideBarElementsItems"
-import SideBarElementsRendered from "./components/SideBarElementsRendered"
+const SideBarElementsItems = lazy(() => import("./components/SideBarElementsItems"))
+const SideBarElementsRendered = lazy(() => import("./components/SideBarElementsRendered"))
 import { useExportImportProvider } from "./hoks/useExportImportProvider"
-import DialogExport from "./components/dialogExport/DialogExport"
+const DialogExport = lazy(() => import("./components/dialogExport/DialogExport"))
 
 function App() {
   const { openEditor } = useEditorProvider()
   const { handleExport } = useExportImportProvider()
   return (
     <div className='grid grid-cols-[270px_1fr_150px]'>
-      <DialogExport/>
+      <Suspense fallback={""}>
+        <DialogExport />
+      </Suspense>
       <aside className='bg-slate-600 w-[270px] h-screen overflow-x-auto'>
-        <div><button type="button" className="m-auto p-2 bg-indigo-600 text-white rounded-lg" onClick={handleExport}>Export</button></div>
+        <div>
+          <button type="button" className="m-auto p-2 bg-indigo-600 text-white rounded-lg" onClick={handleExport}>Export</button>
+        </div>
         <div>
           <div>
-            {!openEditor && <SideBarElementsItems />}
+            <Suspense fallback={"loading"}>
+              {!openEditor && <SideBarElementsItems />}
+            </Suspense>
           </div>
         </div>
-        {openEditor && <EditorTools />}
+        <Suspense fallback={"loading"}>
+          {openEditor && <EditorTools />}
+        </Suspense>
       </aside>
       <main className="h-screen overflow-auto">
         <BuilderArea />
       </main>
-      <SideBarElementsRendered />
+      <Suspense fallback={"loading"}>
+        <SideBarElementsRendered />
+      </Suspense>
     </div>
   )
 }
