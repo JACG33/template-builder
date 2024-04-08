c61b383f7a044b9ae7aadb45b5f5e9470be26d36
JACG33 <casanasjoe@gmail.com>
2024-04-08 15:00:41 -0400

    cambios

--- src/components/sidecontainer/LeftSideContainer.jsx
+++ src/components/sidecontainer/LeftSideContainer.jsx
@@ -4,11 +4,11 @@ import "./leftsidecontainer.css"
 const LeftSideContainer = ({ children, handleShowComponents }) => {
   return (
     <>
-      <div className='aside__float__left__container absolute z-0 px-2 h-screen top-0 left-[50px] bg-slate-700 componets__dialog componets__dialog--show'>
-        <div className="flex justify-end aside__float__left__container__header">
+      <div className='aside__float__left__container componets__dialog componets__dialog--show'>
+        <div className="aside__float__left__container__header">
           <button type='button'
             onClick={handleShowComponents}
-            className='py-1 px-2 bg-red-600 text-white hover:bg-red-500 rounded-lg aside__float__left__container__header__btn'
+            className='aside__float__left__container__header__btn'
           >
             <XCircle />
           </button>
--- src/components/tooltips/ButtonRight.jsx
+++ src/components/tooltips/ButtonRight.jsx
@@ -1,7 +1,7 @@
 import "./buttonright.css"
 const ButtonRight = ({ children, text, handleShowComponents }) => {
   return (
-    <button type="button" className="btn__tooltip m-auto p-2 bg-indigo-600 text-white rounded-lg" onClick={handleShowComponents}>
+    <button type="button" className="btn__tooltip" onClick={handleShowComponents}>
       <div className="btn__tooltip__message">
         {text}
       </div>
